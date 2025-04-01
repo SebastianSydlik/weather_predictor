37697537373b3d63de850d5e61f3b09e7c50bcf8
SebastianSydlik <144226218+SebastianSydlik@users.noreply.github.com>
2025-04-01 19:11:29 +0000

    added transfer to GCP, split off API into separate module

--- get_data.py
+++ get_data.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
-import openmeteo_requests
 import argparse
-import requests_cache
 import pandas as pd
+import weather_API
 
-from retry_requests import retry
 from sqlalchemy import create_engine
 from prefect import flow, task
 from prefect_sqlalchemy import SqlAlchemyConnector
+from prefect.blocks.fields import SecretDict
+from prefect_gcp.cloud_storage import GcsBucket
+from pathlib import Path
 
-@task(log_prints=True)
-def main(params):
-	table_name = params.table_name
-
-	df_raw = get_data_from_api()
-	df = transform_data(df_raw)
-	connection_block = SqlAlchemyConnector.load("sql0")
-	with connection_block.get_connection(begin=False) as engine: 
-		print(df.to_sql(name=f'{table_name}', con=engine, if_exists='replace'))
+@task(retries = 3)
+def extract_data():
+	df = weather_API.get_data_from_api()
+	return df
 
 @task(log_prints=True)
 def transform_data(df):
@@ -27,63 +23,45 @@ def transform_data(df):
 	print(f'\npost: nan Count:\n{df_clean.isnull().sum()}')
 	return df_clean
 
-@task(log_prints=True, retries=3)
-def get_data_from_api():
-	# Setup the Open-Meteo API client with cache and retry on error
-	cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-	openmeteo = openmeteo_requests.Client(session = retry_session)
+@task(log_prints=True)
+def store_data(df, table_name):
+	connection_block = SqlAlchemyConnector.load("postgres-connector")
+	with connection_block.get_connection(begin=False) as engine: 
+		print(df.to_sql(name=f'{table_name}', con=engine, if_exists='replace'))
+		
+@task()
+def store_local(df):
+	"""store dataframe locally as parquet file"""
+	path = Path(f"data/data.parquet")
+	df.to_parquet(path, compression = "gzip")
+	return path
 
-	# Make sure all required weather variables are listed here
-	# The order of variables in hourly or daily is important to assign them correctly below
-	url = "https://archive-api.open-meteo.com/v1/archive"
-	params = {
-		"latitude": 51.26,
-		"longitude": 7.15,
-		"start_date": "2025-01-01",
-		"end_date": "2025-03-06",
-		"hourly": ["temperature_2m", "relative_humidity_2m", "precipitation", "weather_code", "cloud_cover", "wind_speed_100m"],
-		"timezone": "Europe/Berlin"
-	}
-	responses = openmeteo.weather_api(url, params=params)
+@task()
+def write_gcs(path):
+	gcs_block = GcsBucket.load("gcs-connector")
+	gcs_block.upload_from_path(
+		from_path=path,
+		to_path=path
+	)
+	return
 
-	# Process first location. Add a for-loop for multiple locations or weather models
-	response = responses[0]
-	print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+@task()
+def etl_web_to_postgres(params):
+	table_name = params.table_name
 
-	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
-	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-	hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
-	hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
-	hourly_weather_code = hourly.Variables(3).ValuesAsNumpy()
-	hourly_cloud_cover = hourly.Variables(4).ValuesAsNumpy()
-	hourly_wind_speed_100m = hourly.Variables(5).ValuesAsNumpy()
-
-	hourly_data = {"date": pd.date_range(
-		start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-		end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-		freq = pd.Timedelta(seconds = hourly.Interval()),
-		inclusive = "left"
-	)}
-
-	hourly_data["temperature_2m"] = hourly_temperature_2m
-	hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
-	hourly_data["precipitation"] = hourly_precipitation
-	hourly_data["weather_code"] = hourly_weather_code
-	hourly_data["cloud_cover"] = hourly_cloud_cover
-	hourly_data["wind_speed_100m"] = hourly_wind_speed_100m
-
-	df = pd.DataFrame(data = hourly_data)
-	return df
-
+	df_raw = extract_data()
+	df = transform_data(df_raw)
+	path = store_local(df)
+	store_data(df, table_name)
+	write_gcs(path)
+	
 @flow(name='ingest_Flow')
 def call_main(): 
 	parser = argparse.ArgumentParser(description='Ingest latest weather data to Postgres')
 	parser.add_argument('--table_name', help='name of the table where we will write the results to')
 
 	args = parser.parse_args()
-	main(args)
+	etl_web_to_postgres(args)
 
 if __name__ == '__main__':
 	call_main()
---
+++ weather_API.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import openmeteo_requests
+import requests_cache
+
+from retry_requests import retry
+
+
+def get_data_from_api():
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+	openmeteo = openmeteo_requests.Client(session = retry_session)
+
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://archive-api.open-meteo.com/v1/archive"
+	params = {
+		"latitude": 51.26,
+		"longitude": 7.15,
+		"start_date": "2025-01-01",
+		"end_date": "2025-03-06",
+		"hourly": ["temperature_2m", "relative_humidity_2m", "precipitation", "weather_code", "cloud_cover", "wind_speed_100m"],
+		"timezone": "Europe/Berlin"
+	}
+	responses = openmeteo.weather_api(url, params=params)
+
+	# Process first location. Add a for-loop for multiple locations or weather models
+	response = responses[0]
+	print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+
+	# Process hourly data. The order of variables needs to be the same as requested.
+	hourly = response.Hourly()
+	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+	hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+	hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
+	hourly_weather_code = hourly.Variables(3).ValuesAsNumpy()
+	hourly_cloud_cover = hourly.Variables(4).ValuesAsNumpy()
+	hourly_wind_speed_100m = hourly.Variables(5).ValuesAsNumpy()
+
+	hourly_data = {"date": pd.date_range(
+		start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+		end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+		freq = pd.Timedelta(seconds = hourly.Interval()),
+		inclusive = "left"
+	)}
+
+	hourly_data["temperature_2m"] = hourly_temperature_2m
+	hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+	hourly_data["precipitation"] = hourly_precipitation
+	hourly_data["weather_code"] = hourly_weather_code
+	hourly_data["cloud_cover"] = hourly_cloud_cover
+	hourly_data["wind_speed_100m"] = hourly_wind_speed_100m
+
+	df = pd.DataFrame(data = hourly_data)
+	return df
