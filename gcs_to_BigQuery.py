93e1d24f8de638c9b491afe2d1249dee02a82977
SebastianSydlik <144226218+SebastianSydlik@users.noreply.github.com>
2025-04-07 17:02:10 +0000

    changed to pandas_gbq instead of pd for file transfer to BigQuery and added missing parameters, all working fine now.

--- gcs_to_BigQuery.py
+++ gcs_to_BigQuery.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import pandas_gbq
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
@@ -15,7 +16,7 @@ def extract_from_gcs():
     return Path(f"{local_path}{gcs_path}")
 
 @task()
-def transform():
+def transform(path):
     """Data cleaning"""
     df = pd.read_parquet(path)
     print('placeholder for data transformation')
@@ -24,13 +25,13 @@ def transform():
 @task()
 def write_BigQuery(df):
     """Write Dataframe"""
-    gcp_creentials_block = GcpCredentials.load("")
+    gcp_credentials_block = GcpCredentials.load("gcp-creds")
 
-    df.to_gbq(
-        destination = ,
-        project_id = ,
+    pandas_gbq.to_gbq(
+        df, 
+        destination_table = "weather_dataset_0.table0",
+        project_id = "dataengineering-455510",
         credentials = gcp_credentials_block.get_credentials_from_service_account(),
-        chunksize = 500000,
         if_exists="append", 
 
     )
